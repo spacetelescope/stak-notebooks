ed3093d216dfbe678ee1aba52d0c253d6d53edef
Sara Ogaz <ogaz@stsci.edu>
2017-06-06 14:28:54 -0400

    removing build, don't know why I added this in the first place

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,11 +33,9 @@ sys.path.insert(0, os.path.abspath('../..'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 
-import stsci_sphinx_theme
+import stsci_rtd_theme
 
 extensions = [
-    'sphinx_automodapi.automodapi',
-    'sphinx_automodapi.automodsumm',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
 ]
@@ -130,7 +128,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'stsci_sphinx_theme'
+html_theme = 'stsci_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -139,7 +137,7 @@ html_theme = 'stsci_sphinx_theme'
 # html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = [stsci_sphinx_theme.get_html_theme_path()]
+html_theme_path = [stsci_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
